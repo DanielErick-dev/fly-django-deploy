8ae6eeffdece88839abfd303b155340519d9bf7f
Daniel Erick <DanielErick.estudante@gmail.com>
2022-12-27 12:49:34 -0300

    commit initial

--- projeto_principal/wsgi.py
+++ projeto_principal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projectfly project.
+WSGI config for projeto_principal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectfly.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_principal.settings')
 
 application = get_wsgi_application()
